56a7bfadf06b69beffea4b42336a0aeebbae1cf3
Gabriel Ferrari de Oliveira <gbrlferrari@outlook.com>
2025-04-01 21:13:02 -0300

    Configuração de conexão com banco de dados postgres

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 import express from "express";
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize("postgres", "postgres", "Djg@0702", {
+  host: "localhost",
+  port: 5432,
+  dialect: "postgres",
+  define: {
+    timestamps: false,
+    freezeTableName: true,
+  },
+});
+
+try {
+  await sequelize.authenticate();
+  console.log("Conexão com o banco estabelecida com sucesso.");
+} catch (error) {
+  console.error("Não foi possível estabelecer uma conexão com o banco:", error);
+}
 
 const app = express();
 app.use(express.json());
